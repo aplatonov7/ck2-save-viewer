2ea51b21f882f4ce1dcbe89421bfcce8bb321673
Artem Platonov <discodesu@gmail.com>
2017-12-17 01:59:24 +0400

    Prettier all the things

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,14 +3,11 @@ const isDev = process.env.NODE_ENV === 'development'
 const getTimestamp = () => {
   const date = new Date()
   return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-} 
+}
 
 const logger = (...args) => {
   if (!isDev) return
   console.log(getTimestamp(), ...args)
 }
 
-export {
-  logger,
-  isDev
-}
+export { logger, isDev }
